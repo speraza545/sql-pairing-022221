fd5527abcecb157bdb84ad54d1ee5358f9a28973
speraza545 <s.peraza545@gmail.com>
2021-03-24 12:04:11 -0400

    some answers added in exercise 1. 1-19 answered

--- SQL-exercise/SQL_exercise_01/1_questions.sql
+++ SQL-exercise/SQL_exercise_01/1_questions.sql
@@ -1,14 +1,24 @@
 -- LINK: https://en.wikibooks.org/wiki/SQL_Exercises/The_computer_store
 -- 1.1 Select the names of all the products in the store.
+    -- SELECT products.name FROM products;
 -- 1.2 Select the names and the prices of all the products in the store.
+    -- SELECT products.name, products.price FROM products;
 -- 1.3 Select the name of the products with a price less than or equal to $200.
+    -- SELECT products.name FROM products WHERE price <= 200;
 -- 1.4 Select all the products with a price between $60 and $120.
+    -- SELECT * FROM products WHERE price BETWEEN 60 AND 120;
 -- 1.5 Select the name and price in cents (i.e., the price must be multiplied by 100).
+    -- SELECT products.name, products.price * 100 FROM products;
 -- 1.6 Compute the average price of all the products.
+    -- SELECT AVG(products.price) FROM products;
 -- 1.7 Compute the average price of all products with manufacturer code equal to 2.
+    -- SELECT AVG(products.price) FROM products WHERE manufacturer = 2;
 -- 1.8 Compute the number of products with a price larger than or equal to $180.
+    -- SELECT count(*) from products where price >= 180;
 -- 1.9 Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
+    -- SELECT products.name, products.price FROM products WHERE price >= 180 ORDER BY price DESC, name ASC;
 -- 1.10 Select all the data from the products, including all the data for each product's manufacturer.
+    -- SELECT * FROM products;
 -- 1.11 Select the product name, price, and manufacturer name of all the products.
 -- 1.12 Select the average price of each manufacturer's products, showing only the manufacturer's code.
 -- 1.13 Select the average price of each manufacturer's products, showing the manufacturer's name.
